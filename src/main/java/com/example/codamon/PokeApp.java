5b4c6d00649a8ccfef9fe798a24833c3aae618db
Azhega <lethan986@gmail.com>
2025-03-01 20:33:56 +0100

    feat : Preview FXML One Pokémon in TeamBuilder

--- src/main/java/com/example/codamon/PokeApp.java
+++ src/main/java/com/example/codamon/PokeApp.java
@@ -1,22 +1,37 @@
 package com.example.codamon;
 
+import com.example.codamon.models.SceneName;
+import com.example.codamon.views.BattleView;
+import com.example.codamon.views.MenuView;
+import com.example.codamon.views.TeamBuilderView;
 import com.example.codamon.core.*;
 
 import com.example.codamon.core.TurnManager.ConsoleTurnManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PokeApp extends Application {
+
+    private static Map<SceneName, Scene> scenes = new HashMap<>();
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PokeApp.class.getResource("views/Poke-View.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Codamon");
-        stage.setScene(scene);
+        scenes.put(
+                SceneName.MENU, new MenuView(stage).getScene());
+        scenes.put(
+                SceneName.BATTLE, new BattleView(stage).getScene());
+        scenes.put(
+                SceneName.TEAMBUILDER, new TeamBuilderView(stage).getScene());
+
+        stage.setScene(scenes.get(SceneName.MENU));
+        stage.setTitle("MENU");
         stage.show();
 
 
@@ -35,6 +50,10 @@ public class PokeApp extends Application {
         //System.out.println(team1);System.out.println(team2);
         System.out.println(battle.activesPokeomToString());
 
+    }
+
+    public static Map<SceneName, Scene> getScenes() {
+        return scenes;
     }
 
     public static void main(String[] args) {
